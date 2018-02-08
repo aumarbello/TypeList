8ee21c14000e87a38523614005f00ce367fe6c63
Ahmed Umar <khattabu@gmail.com>
2018-02-08 13:02:16 +0100

    Implemented app.py

--- app/app.py
+++ app/app.py
@@ -1,26 +1,26 @@
-# from flask import Flask, jsonify, request
-#
-#
-# app = Flask(__name__)
-#
-#
-# testing_types = [
-#     {'name': 'unit testing', 'description': 'testing individual units of a source code'}
-# ]
-#
-#
-# @app.route('/tests')
-# def get_test_types():
-#     return jsonify(testing_types)
-#
-#
-# @app.route('/tests', methods=['POST'])
-# def add_test_type():
-#     testing_types.append(request.get_json())
-#     return "SuccessFully Added Type", 204
-#
-#
-# if __name__ == '__main__':
-#     app.run(debug=True)
-#
-#
+from flask import Flask, jsonify, request
+
+
+app = Flask(__name__)
+
+
+testing_types = [
+    {'name': 'unit testing', 'description': 'testing individual units of a source code'}
+]
+
+
+@app.route('/tests')
+def get_test_types():
+    return jsonify(testing_types)
+
+
+@app.route('/tests', methods=['POST'])
+def add_test_type():
+    testing_types.append(request.get_json())
+    return "SuccessFully Added Type", 204
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
